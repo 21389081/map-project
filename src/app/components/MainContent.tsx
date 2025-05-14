062c32fdade12fda75a260f0e0a719c6170a44ee
vincentchen <a978090@gmail.com>
2025-05-14 23:12:13 +0800

    環境建置

---
+++ src/app/components/MainContent.tsx
@@ -0,0 +1,11 @@
+import Select from './Select';
+import Map from './Map';
+export default function MainContent() {
+    return (
+        <div className=''>
+            MainContent
+            <Select />
+            <Map />
+        </div>
+    );
+}

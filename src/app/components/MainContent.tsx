1c936c6fcb2513f6cbf27e559230fa819b909e39
vincentchen <a978090@gmail.com>
2025-05-20 00:28:53 +0800

    地圖初始化

--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -1,5 +1,10 @@
+'use client';
+import dynamic from 'next/dynamic';
 import Select from './Select';
-import Map from './Map';
+// 引用元件並關閉伺服器渲染
+const Map = dynamic(() => import('./Map'), {
+    ssr: false, // 關閉伺服器渲染
+});
 export default function MainContent() {
     return (
         <div className='flex min-h-screen min-w-screen'>
--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,3 +1,25 @@
-export default function Map() {
-    return <div>Map</div>;
+import { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default function LeafletMap() {
+    const mapRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!mapRef.current) return;
+
+        // 初始化地圖
+        const map = L.map(mapRef.current).setView([25.033964, 121.564468], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors',
+        }).addTo(map);
+
+        // 若要之後操作地圖，可把 map 存到 ref.current
+        return () => {
+            map.remove();
+        };
+    }, []);
+
+    return <div ref={mapRef} className='w-full h-screen' />;
 }
